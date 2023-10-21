10bd7b8aab6ecef410a95b4616e3f30421679fa0
caitlincorrigan <caitlinfcorrigan@gmail.com>
2023-10-21 16:17:30 -0700

    begins implementing add ticket functionality WIP

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -1,25 +1,28 @@
 const Ticket = require('../models/ticket');
 
 module.exports = {
-    new: newTicket
+    new: newTicket,
+    create
 
 }
 
-// async function create(req, res) {
-//     // No trims or other data cleansing functions bc of form styling & db req
-//     for (let key in req.body) {
-//         if (req.body[key] === '') delete req.body[key];
-//     }
-//     try {
-//         await Flight.create(req.body);
-//         // Redirect after CRUDing data
-//         res.redirect('/flights');
-//     } catch (err) {
-//         // Typically a validation error
-//         console.log(err);
-//         res.render('flights/new', { errorMsg: err.message});
-//     }
-// }
+// Refactor to update tickets
+async function create(req, res) {
+    console.log(req);
+    for (let key in req.body) {
+        if (req.body[key] === '') delete req.body[key];
+    }
+    try {
+        console.log(req.body)
+        await Ticket.create(req.body);
+        // Redirect after CRUDing data
+        res.redirect('/flights/:id');
+    } catch (err) {
+        // Typically a validation error
+        console.log(err);
+        res.render('flights/:id/tickets/new', { errorMsg: err.message});
+    }
+}
 
 function newTicket(req, res) {
     console.log("hit tickets controller")
--- routes/tickets.js
+++ routes/tickets.js
@@ -4,13 +4,11 @@ const router = express.Router();
 // Add controller for tickets
 const ticketsCtrl = require('../controllers/tickets');
 
-console.log("loaded ticket router")
-
 // Route to new ticket page
 router.get('/flights/:id/tickets/new', ticketsCtrl.new);
 
 // Route to submit new ticket
 // POST /flights/:id/tickets/new
-// router.post('/flights/:id/tickets/new', ticketsCtrl.create);
+router.post('/flights/:id/', ticketsCtrl.create);
 
 module.exports = router;
